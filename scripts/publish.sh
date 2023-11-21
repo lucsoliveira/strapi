90339ec5f9f8a4f6f51be1688d9147e8ed4f7e06
Lucas de Oliveira Pereira <olucas1998@gmail.com>
2023-11-21 17:14:43 -0300

    fix: publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,11 +5,17 @@ cd "$(dirname "$0")/.."
 
 set -e
 
+npm_server=""
 npm_email=""
 npm_token=""
 
+echo "Please enter npm server (without http)"
+read -r npm_server
+
+
 echo "Please enter npm email"
 read -r npm_email
+
 
 echo "Please enter npm token"
 read -r npm_token
@@ -17,10 +23,10 @@ read -r npm_token
 # save npmrc configs
 echo "
 registry=https://registry.npmjs.com/
-@strapi:registry=http://localhost:8081/repository/npm-internal/
+@strapi:registry=http://$npm_server/
 email=$npm_email
 always-auth=true
-//localhost:8081/repository/npm-internal/:_auth=$npm_token
+//$npm_server/:_auth=$npm_token
 " > .npmrc
 
 # publish packages
